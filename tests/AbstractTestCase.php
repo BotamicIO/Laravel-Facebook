344f0a2da063d6ecd39fbb32cd62b87a8022b69c
Brian Faust <faustbrian@users.noreply.github.com>
2016-12-03 08:38:39 +0200

    Apply fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Facebook.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Tests\Facebook;
 
 use BrianFaust\Facebook\ServiceProvider;
